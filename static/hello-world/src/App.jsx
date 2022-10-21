7912b466d0ea27db8d6aaa2fc385d8f414f912e0
THANGNV <thangviet222@gmail.com>
2022-10-20 21:22:37 -0700

    second

--- static/hello-world/src/App.jsx
+++ static/hello-world/src/App.jsx
@@ -6,15 +6,17 @@ const styles = {
     padding: 20, textAlign: "center",
 };
 
+const issueLinkName = `blocks`;
 const data = async () => {
-    const params = 'issueLinkType != "is blocked by" or issueLinkType = null';
+    const params = `issueLinkType = "${issueLinkName}" or issueLinkType = null`;
     const response = await requestJira(`/rest/api/2/search?jql=${params}`);
     console.log('call api jira');
     return await response.json();
 };
+
 const issueData = data().then((result) => {
     let data = [];
-
+    console.log(result)
     result.issues.forEach((element) => {
         data.push({
             label: element.key, value: element.key, visible: true,
@@ -23,6 +25,29 @@ const issueData = data().then((result) => {
 
     return data;
 });
+
+const createIssueLink = async (outwardIssue, inwardIssue) => {
+    let body = `{
+  "outwardIssue": {
+    "key": "${outwardIssue}"
+  },
+  "inwardIssue": {
+    "key": "${inwardIssue}"
+  },
+  "type": {
+    "name": "Blocks"
+  }
+}`;
+    const response = await requestJira(`/rest/api/2/issueLink`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: body
+    });
+    return response.status;
+}
 const App = () => {
     let [treeData, setTreeData] = useState();
 
@@ -32,17 +57,20 @@ const App = () => {
         }
     });
     console.log('reload plugin');
-    return (<div style={styles}>
+    return (
         <Tree
+            height={900}
             data={treeData}
             draggable
             defaultExpandAll
-            onDrop={({createUpdateDataFunction}, event) => {
-                setTreeData(createUpdateDataFunction(treeData))
+            onDrop={({createUpdateDataFunction, dropNode, dragNode}, event) => {
+                createIssueLink(dragNode.value, dropNode.value).then(result => {
+                    if (result === 201)
+                        setTreeData(createUpdateDataFunction(treeData))
+                })
             }
             }
-        />
-    </div>);
+        />);
 };
 
 export default App;
